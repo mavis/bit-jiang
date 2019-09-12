4f576e5c173f7905fedf5122250236d73ee9ba04
zhaoyiqing <zhaoyiqing@oradt.com>
2019-09-12 13:08:48 +0800

    init
    Change-Id: I519c21eef093863379a4a7abdb21559c7cd0aaff

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">
+        <div class="main-content">
+                111
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
